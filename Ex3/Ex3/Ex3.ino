8cc63c19ab82aa7f70885b3cfadc442eb8cd972e
carlosmartins77 <carlosmatrins@gmail.com>
2022-01-11 16:15:37 +0000

    UPDATE Ex3.ino

--- Ex3/Ex3/Ex3.ino
+++ Ex3/Ex3/Ex3.ino
@@ -3,14 +3,20 @@
 
 #define subir 34935 //Botao 0
 #define descer 8415 //Botao 1
-#define parar 41055 //Botao 2
 
-int receiver_pin = 11;
-Servo motor;
-int pos;
-int buttonPin = 3;
-int state = 0;
+int receiver_pin = 11; // IR Sensor
+Servo motor; // Servo
+int pos; // Posiçao para o SERVO (Usada para definir a velocidade do servo)
+
+int buttonPin = 3; // Botao suspender movimento
 unsigned long lastInterrupt;
+int state = 0;
+
+/* Sensor distancia*/
+int trigPin = 8;
+int echoPin = 7;
+long duration, distance; // Duration used to calculate distance
+/* ---- */
 
 IRrecv receiver(receiver_pin);
 decode_results output;
@@ -19,15 +25,30 @@ decode_results output;
 void setup()
 {
   Serial.begin(9600);
+
+  /* Sensor distancia*/
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  /* ---- */
+
+  /* Infravermelhos */
   receiver.enableIRIn();
-  motor.attach(9);
+  /* ---- */
+
+  motor.attach(9); //Ligar o SERVO à porta 9
   pinMode(buttonPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(buttonPin), suspender, HIGH);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), suspender, FALLING);
 }
 
 void loop()
 {
-  
+  digitalWrite(trigPin, HIGH); //Enviar onde ultrassonica
+  delayMicroseconds(10); //Durante 10ms
+  digitalWrite(trigPin, LOW); //Para de enviar onda
+  duration = pulseIn(echoPin, HIGH);
+  //Calculate the distance (in cm) based on the speed of sound.
+  distance = (duration / 58.2) * 2;
+
   if (receiver.decode(&output))
   {
     unsigned int value = output.value;
@@ -39,25 +60,12 @@ void loop()
         receiver.resume();
 
       case subir:
-      
-        for (pos = 90; pos <= 180; pos += 1)
-        {
-            motor.write(pos);
-            delay(15);
-        }
+
+        subirBarra();
         break;
-      
+
       case descer:
-        for (pos = 180; pos >= 90; pos -= 1)
-        {
-          motor.write(pos);
-          delay(15);
-        }
-        break;
-      
-      case parar:
-        //motor.detach();
-    //attachInterrupt(receiver_pin, teste, CHANGE);
+        descerBarra();
         break;
     }
 
@@ -65,13 +73,47 @@ void loop()
 }
 
 void suspender() {
-   
-  state = !state;
-    Serial.println(state);
-    if (state == 1)
-        motor.detach();
-    else
-        motor.attach(9);
 
-//motor.attach(9);
+  state = !state;
+  Serial.println(state);
+  if (state == 1)
+    motor.detach();
+  else
+    motor.attach(9);
+
+  //motor.attach(9);
+}
+
+void subirBarra()
+{
+  for (pos = 0; pos <= 180; pos += 1)
+  {
+    motor.write(pos);
+    delay(15);
+  }
+}
+
+
+void descerBarra()
+{
+
+  for (pos = 180; pos >= 0; pos -= 1)
+  {
+    digitalWrite(trigPin, HIGH); //Enviar onde ultrassonica
+    delayMicroseconds(10); //Durante 10ms
+    digitalWrite(trigPin, LOW); //Para de enviar onda
+    duration = pulseIn(echoPin, HIGH);
+    //Calculate the distance (in cm) based on the speed of sound.
+    distance = (duration / 58.2) * 2;
+
+    if (distance > 200) {
+      motor.write(pos);
+      delay(15);
+    }
+    else if (distance <= 200) {
+
+      subirBarra();
+      break;
+    }
+  }
 }
